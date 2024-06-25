f8e10e7b58a3543330bebc3055955ae5cb696c1d
beenarani <ranibeena187@gmail.com>
2024-06-25 20:40:25 +0100

    My homework sw 6

---
+++ src/main/java/com/nopcommerce/demo/pages/ComputerPage.java
@@ -0,0 +1,31 @@
+package com.nopcommerce.demo.pages;
+
+/**
+ * Computers text, DesktopsLink, NotebooksLink, SoftwareLink Locators and create appropriate methods for it.
+ */
+import com.nopcommerce.demo.utility.Utility;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
+public class ComputerPage extends Utility {
+    private static final Logger log = LogManager.getLogger(HomePage.class.getName());
+    @CacheLookup
+    @FindBy(linkText = "Computers")
+    WebElement computersText;
+    @CacheLookup
+    @FindBy(xpath = "//a[text()=' Desktops ']")
+    WebElement desktopLink;
+
+    public String getComputerText() {
+        log.info("Get computers text" + computersText.toString());
+        return getTextFromElement(computersText);
+    }
+
+    public void clickOnDesktop() {
+        log.info("Click on desktop" + desktopLink.toString());
+        clickOnElement(desktopLink);
+    }
+}
---
+++ src/main/java/com/nopcommerce/demo/pages/DesktopsPage.java
@@ -0,0 +1,33 @@
+package com.nopcommerce.demo.pages;
+
+/**
+ * Desktops text, Sortby, Display, selectProductList Locators and it's actions
+ */
+import com.nopcommerce.demo.utility.Utility;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
+public class DesktopsPage extends Utility {
+    private static final Logger log = LogManager.getLogger(HomePage.class.getName());
+
+    @CacheLookup
+    @FindBy(linkText = "Desktops")
+    WebElement desktopText;
+    @CacheLookup
+    @FindBy(linkText = "Build your own computer")
+    WebElement buildYourOwnComputer;
+
+    public String getDesktopsText() {
+        log.info("Get desktop text" + desktopText.toString());
+        return getTextFromElement(desktopText);
+    }
+
+    public void clickOnBuildYourOwnComputer() {
+        clickOnElement(buildYourOwnComputer);
+        log.info("Clicking on build your own computer link : " + buildYourOwnComputer.toString());
+
+    }
+}
